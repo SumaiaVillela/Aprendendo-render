760deb53e0283384757be5cc58cfc37c374d3ed5
SumaiaVillela <dadosparasumaia@gmail.com>
2024-03-20 22:20:18 -0300

    Adicionando o robô do Telegram

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 @app.route("/")
 def index():
@@ -9,3 +12,12 @@ def index():
 @app.route("/teste")
 def teste():
     return "Essa página aqui é um <b>teste</b>!"
+
+@app.route("/telegram", methods=["POST"])
+def telegram_update():
+    update = request.json
+    url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}sendMessage"
+    chat_id = update["message"]["chat"]["id"]
+    mensagem = {"chat_id" : chat_id, "text": "mensagem <b>recebida</b>!", "parse_mode": "HTML"}
+    requests.post(url_envio_mensagem, data=mensagem)
+    return "ok"
